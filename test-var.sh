7065690552156fc126de58be83a25f66762c5e6f
David Bradshaw <55761303+davidrbradshaw@users.noreply.github.com>
2020-09-10 01:39:46 -0700

    Add files via upload

---
+++ test-var.sh
@@ -0,0 +1,16 @@
+# ----------------
+# test-var.sh file
+# ----------------
+#!/bin/bash
+
+inputFile=${OHPC_INPUT_LOCAL_VAR:-/opt/ohpc/pub/doc/recipes/centos7/input-var.local}
+
+if [ ! -e ${inputFile} ];then
+	echo "Error: Unable to access local input file -> ${inputFile}"
+	exit 1
+else
+	. ${inputFile} || { echo "Error sourcing ${inputFile}"; exit 1; }
+fi
+
+echo "testing:"
+echo "$var"
